0d6c8034228a0d8c24c7ea0c3db2b0eb40a944e9
Owen Campbell <owen.campbell@empiria.co.uk>
2015-07-03 16:58:18 +0100

    adds nodejs installation

---
+++ scripts/nodejs/install_nodejs.sh
@@ -0,0 +1,14 @@
+apt-get -y install nodejs npm nodejs-legacy
+npm install
+npm install -g bower
+su -c "bower install -s" vagrant
+npm install -g gulp
+
+# configure Gulp as an upstart daemon
+cp /tmp/gulp-server.conf /etc/init
+if (( $(ps -ef | grep -v grep | grep "gulp" | wc -l) > 0 ))
+then
+    restart gulp-server
+else
+    start gulp-server
+fi
