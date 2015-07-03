6eaac1dfad1ca8e3e04a70ba35cd88416c23edc3
Owen Campbell <owen.campbell@empiria.co.uk>
2015-07-03 16:47:57 +0100

    adds packages to initial.sh

--- scripts/base/initial.sh
+++ scripts/base/initial.sh
@@ -8,7 +8,7 @@ dpkg-reconfigure -f noninteractive tzdata
 
 apt-get update
 apt-get -y upgrade
-apt-get -y install linux-headers-$(uname -r) git curl pkg-config
+apt-get -y install linux-headers-$(uname -r) git curl pkg-config libfreetype6-dev libpng12-dev redis-server
 
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
 sed -i -e 's/%sudo  ALL=(ALL:ALL) ALL/%sudo  ALL=NOPASSWD:ALL/g' /etc/sudoers
